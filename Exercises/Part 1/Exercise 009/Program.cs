8ace676442374a85045e52c14be58202c39f70a6
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-10-23 21:53:36 +0300

    23.10.2020

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -6,7 +6,12 @@ namespace exercise_09
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("Hello, how are you?");
+            string Fine,thanks = Console.ReadLine();
+            Console.WriteLine("That's interesting, tell me more");
+            string Ilearncoding = Console.ReadLine();
+              Console.WriteLine("Thank you for sharing!");
+            
 
         }
     }
