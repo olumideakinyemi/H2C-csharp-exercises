b2497d900cfc2f346e38a5471186b12033692063
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-02 10:46:45 +0200

    02.11.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -6,7 +6,12 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("I will tell a story, but I need some information. Give a name for main character:");
+            string Ada = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            string DataScientist = Console.ReadLine();
+            Console.WriteLine("Here is the story:Once upon a time there was a Data Scientist called Ada \nOn her way to work, Ada often pondered what being Data Scientist meant to them.\nWhen you work as a Data Scientist you meet interesting people.\nAda enjoys their work as Data Scientist, The end."); 
+            
 
         }
     }
