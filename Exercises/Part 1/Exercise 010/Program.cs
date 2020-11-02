6d4e7562d7c47b2424326a830436bf2829920e33
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-02 15:04:06 +0200

    02.11.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -10,8 +10,10 @@ namespace exercise_10
             string Ada = Console.ReadLine();
             Console.WriteLine("Give the character a profession:");
             string DataScientist = Console.ReadLine();
-            Console.WriteLine("Here is the story:Once upon a time there was a Data Scientist called Ada \nOn her way to work, Ada often pondered what being Data Scientist meant to them.\nWhen you work as a Data Scientist you meet interesting people.\nAda enjoys their work as Data Scientist, The end."); 
-            
+            Console.WriteLine("Here is the story:Once upon a time there was a Data Scientist called Ada\n" + 
+                "On her way to work, Ada often pondered what being Data Scientist meant to them.\n" +  
+                "When you work as a Data Scientist you meet interesting people.\n" + 
+                "Ada enjoys their work as Data Scientist, The end.");
 
         }
     }
