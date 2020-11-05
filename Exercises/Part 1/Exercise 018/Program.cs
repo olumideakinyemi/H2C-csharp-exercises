bcac6cfc204031fa06bfb5a565493381754d1ed2
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-05 21:40:05 +0200

    05.11.2020

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,8 +7,16 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine("Give the first number!");
+      string userinput = Console.ReadLine();
+      int FirstintValue = Convert.ToInt32(userinput);
+      Console.WriteLine ("Give the second number!");
+      string secondUserinput = Console.ReadLine();
+      int SecondintValue = Convert.ToInt32(secondUserinput);
+      Console.WriteLine ("Give the third number!");
+      string thirduserinput = Console.ReadLine();
+      int ThirdintValue = Convert.ToInt32(thirduserinput);
+      Console.WriteLine ("The sum is " + (FirstintValue + SecondintValue + ThirdintValue));
     }
   }
 }
