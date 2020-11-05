4f9a412022a504743f821427e8f2a157f7322fff
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-05 12:05:48 +0200

    05.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -11,19 +11,16 @@ namespace exercise_15
       string message = Console.ReadLine();
       Console.WriteLine("Give an integer:");
       string userInput = Console.ReadLine();
-      int intValue = Convert.ToInt32(userInput);
-      Console.WriteLine(intValue); 
-      //Console.WriteLine("Give a double:");
-      //string inputDoubleValue = Console.ReadLine();
-      //double doubleValue = Convert.ToDouble(inputDoubleValue);
-      //Console.WriteLine = (doubleValue);
+      int intValue = Convert.ToInt32(userInput); 
+      Console.WriteLine("Give a double:");
+      string inputValue = Console.ReadLine();
+      double doubleValue = Convert.ToDouble(inputValue);
       Console.WriteLine("Give a boolean:");
       string True = Console.ReadLine();
       bool booleanValue = System.Convert.ToBoolean(True);
-      Console.WriteLine(booleanValue);
       Console.WriteLine ("Your string: " + message);
-      Console.WriteLine ("Your integer " + intValue);
-      //Console.WriteLine ("Your double " + doubleValue);
+      Console.WriteLine ("Your integer: " + intValue);
+      Console.WriteLine ("Your double: " + doubleValue);
       Console.WriteLine ("Your boolean: " + booleanValue);
 
     }
