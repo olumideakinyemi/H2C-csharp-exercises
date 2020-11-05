4affe68e72cace3f5a68c97f292e9e0fbffb1354
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-05 11:23:45 +0200

    05.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give a string:");
+      string message = Console.ReadLine();
+      Console.WriteLine("Give an integer:");
+      string userInput = Console.ReadLine();
+      int intValue = Convert.ToInt32(userInput);
+      Console.WriteLine(intValue); 
+      //Console.WriteLine("Give a double:");
+      //string inputDoubleValue = Console.ReadLine();
+      //double doubleValue = Convert.ToDouble(inputDoubleValue);
+      //Console.WriteLine = (doubleValue);
+      Console.WriteLine("Give a boolean:");
+      string True = Console.ReadLine();
+      bool booleanValue = System.Convert.ToBoolean(True);
+      Console.WriteLine(booleanValue);
+      Console.WriteLine ("Your string: " + message);
+      Console.WriteLine ("Your integer " + intValue);
+      //Console.WriteLine ("Your double " + doubleValue);
+      Console.WriteLine ("Your boolean: " + booleanValue);
 
     }
   }
