4867a1ae61736a2462cafd7fc6f9c135b3e83327
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-06 10:47:41 +0200

    06.11.2020

--- Exercises/Part 1/Exercise 020/Program.cs
+++ Exercises/Part 1/Exercise 020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace exercise_20
 {
@@ -7,8 +8,14 @@ namespace exercise_20
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+      Console.WriteLine ("Give the first number!");
+      string userInput = Console.ReadLine();
+      int FirstintValue = Convert.ToInt32(userInput);
+      Console.WriteLine ("Give the second number!");
+      string SeconduserInput = Console.ReadLine();
+      int SecondintValue = Convert.ToInt32(SeconduserInput);
+      int multiply = FirstintValue * SecondintValue;
+      Console.WriteLine ( FirstintValue + " * " + SecondintValue + " = " + multiply);
     }
   }
 }
