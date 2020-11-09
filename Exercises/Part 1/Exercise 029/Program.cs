5dcdc81ff20a5d219035ccd1a72e27c0e8a6e788
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-09 10:40:50 +0200

    09.11.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine ("Give the first number!");
+      string userinput = Console.ReadLine();
+      int intValue = Convert.ToInt32(userinput);
+      Console.WriteLine ("Give the second number!");
+      string SeconduserInput = Console.ReadLine();
+      int SecondintValue = Convert.ToInt32(SeconduserInput);
+if(intValue > SecondintValue)
+{
+      Console.WriteLine ("The larger number is " + intValue + "!");      
+}
+else if (SecondintValue > intValue)
+{
+      Console.WriteLine ("The larger number is " + SecondintValue + "!");    
+}
+else
+{
+     Console.WriteLine ("They are equal!");       
+}
 
     }
   }
