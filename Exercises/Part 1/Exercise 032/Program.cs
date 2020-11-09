e6876b0db45a14f819bb124b2d2e8bef399b8bc4
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-09 13:02:23 +0200

    09.11.2020

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_32
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Speak, friend, and enter!");
+      string userInput = Console.ReadLine();
+      string Mellon = "Mellon";
+if (userInput == Mellon)     
+{
+     Console.WriteLine ("Welcome, friend");       
+}
+else
+{
+      Console.WriteLine ("They've got a cave troll!");     
+}
 
     }
   }
