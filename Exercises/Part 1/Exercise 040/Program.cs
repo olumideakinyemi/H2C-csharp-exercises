3a13cbbb55115562525a17094be392899374fa94
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-11 11:05:12 +0200

    11.11.2020

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -7,7 +7,31 @@ namespace exercise_40
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      int count = 0;
+      int sum = 0;
+while (true)
+{
+      Console.WriteLine("Give a number:");
+      int number = Convert.ToInt32(Console.ReadLine());
+      if (number == 0) 
+    {
+       break;         
+    }       
+     if (number > 0)
+    {
+       count = count + 1;         
+       sum = sum + number;
+    }
+     if (number < 0 )
+    {
+      count = count + 1;          
+       sum = sum + number;         
+    }
+                                  
+}
+Console.WriteLine("Total sum of numbers: " + sum);
+Console.WriteLine("Total amount of numbers: " + count);
+
     }
   }
 }
