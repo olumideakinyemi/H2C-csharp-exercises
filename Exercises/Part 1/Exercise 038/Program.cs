95a0dd36cdf42b0e56d32d5a56d673734ddd974b
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-11 10:12:54 +0200

    11.11.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,8 +6,23 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      int countNegativeNo = 0;
+while (true)
+{
+      Console.WriteLine("Give a number:");
+      int number = Convert.ToInt32(Console.ReadLine());
+      if (number == 0)
+    {
+        break;        
+    }
+      if (number < 0)
+    {
+        countNegativeNo = countNegativeNo + 1;        
+    }
+            
+            
+}
+Console.WriteLine("Total amount of negative numbers: " + countNegativeNo);
     }
   }
 }
