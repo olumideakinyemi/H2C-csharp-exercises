b815969a2957ab07e8b6884da3d0a79b3b4fe0ce
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-11 09:18:38 +0200

    11.11.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,27 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+while (true)
+{
+     Console.WriteLine("Give a number:");
+     int number = Convert.ToInt32(Console.ReadLine());
+     if (number == 0)
+     {
+          break;      
+     }
+     if (number < 0)      
+     {
+        Console.WriteLine ("That is negative");       
+        continue;
+     }      
+     if (number > 0)
+     {
+        Console.WriteLine(number * number);       
+     }
+            
+                       
+}
+//Console.WriteLine("All done!");
     }
   }
 }
