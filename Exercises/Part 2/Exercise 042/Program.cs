38e844cd2af336968c087f6d5f99bcda56479ba1
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-13 09:37:16 +0200

    13.11.2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -7,8 +7,11 @@ namespace exercise_42
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-    
+      int FirstuserInput = Convert.ToInt32(Console.ReadLine());
+      int SeconduserInput = Convert.ToInt32(Console.ReadLine());
+      int sum = ( FirstuserInput + SeconduserInput);
+      double squareRoot = Math.Sqrt(sum);
+      Console.WriteLine(squareRoot);
     }
   }
 }
