9e3df651d31696ce257d7d91f51f3e0f507eca86
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-13 10:20:59 +0200

    13.11.2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      int FirstuserInput = Convert.ToInt32(Console.ReadLine());
+      int SeconduserInput = Convert.ToInt32(Console.ReadLine());
+      if (FirstuserInput > SeconduserInput)
+      {
+         Console.WriteLine ( FirstuserInput + " is grEater than " + SeconduserInput + "." );
+      }
+      else if (FirstuserInput < SeconduserInput)
+      {
+         Console.WriteLine (FirstuserInput + " is less than " + SeconduserInput + ".");  
+      }
+      else
+      {
+         Console.WriteLine (FirstuserInput + " is equal to " + SeconduserInput + "." );
+      }
 
     }
   }
