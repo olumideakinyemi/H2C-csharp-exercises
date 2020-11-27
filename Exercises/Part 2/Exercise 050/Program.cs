9ddd228e1d1e2d1c576053432617436c646ea0bf
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-27 10:51:37 +0200

    27.11.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,12 +7,20 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+      Console.WriteLine("How many times?");
+      int userInput = Convert.ToInt32(Console.ReadLine());
+      int numOftimes = 0;
+      while(userInput > numOftimes)
+      {
+        PrintPhrase();
+        numOftimes++;
+      }  
  
-    }
-
-    // Write your method here:
+    } // Write your method here:
     public static void PrintPhrase() {
-
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");  
+    }
     }
   }
 }
