762a147ea5ef670fc97e399bbe55a2020fa9d311
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-27 22:21:15 +0200

    27.11.2020

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -7,15 +7,17 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-      Division(6,3);
-
+      int numerator = Convert.ToInt32(Console.ReadLine());
+      int denomirator = Convert.ToInt32(Console.ReadLine());
+      double divresult = Division(numerator,denomirator);
+      Console.WriteLine(divresult);
     }
 
     // Write your method here:
-    public static void Division(int numerator, int denomerator){
+    public static double Division(int numerator, int denomerator){
     {
-      double divide = (numerator/denomerator) * 1.0;
-      Console.WriteLine(divide);   
+      double divide = ((double) numerator/ (double) denomerator);
+      return divide;   
     
     }    
         
