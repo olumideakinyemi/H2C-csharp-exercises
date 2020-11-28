7fa12ed94f4729c842053c34349aea61f81ee4ec
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-28 22:13:39 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -6,22 +6,32 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
-
+         PrintStars(5);
+         PrintStars(3);
+         PrintStars(9);
+        
     }
 
 
     public static void PrintStars(int number)
     {
+      int i = 0;
+      while (i < number)
+      {
       // you can print one star with the command
-      // Console.Write("*");
+      Console.Write("*");
       // call the print command n times
+      i++;
+      }
       // in the end print a line break with the comand
-      // Console.WriteLine("");
+      Console.WriteLine("");
     }
 
     public static void PrintSquare(int size)
     {
-
+      
+     
+      
     }
 
     public static void PrintRectangle(int width, int height)
