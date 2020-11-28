36917140daf395fed2410e40a0b57a77198b251a
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-11-28 12:48:30 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 057/Program.cs
+++ Exercises/Part 2/Exercise 057/Program.cs
@@ -7,7 +7,9 @@ namespace exercise_57
     public static int Sum(int number1, int number2, int number3, int number4)
     {
       // write your code here
+      int sum = number1 + number2 + number3 + number4;
       // remember to include return (at the end)!
+      return sum;
     }
 
     public static void Main(String[] args)
