ca1a6698dcb41e1431f8a0a15f20161c4a329c21
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-12-10 10:46:27 +0200

    10.12.2020

--- Exercises/Part 3/Exercise 070/Program.cs
+++ Exercises/Part 3/Exercise 070/Program.cs
@@ -17,6 +17,16 @@ namespace exercise_70
         }
         list.Add(input);
       }
+      int grEatest = list[0];
+
+      for(int i = 0; i < list.Count; i++) {
+       int number = list[i];
+       if (grEatest < number) {
+        grEatest = number;
+       }
+      }
+
+       Console.WriteLine("The grEatest number: " + grEatest);
     }
   }
 }
