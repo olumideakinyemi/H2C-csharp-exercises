0db6eb93c4a69b778223e6f896460fd4d6604157
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2020-12-16 12:35:03 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 078/Program.cs
+++ Exercises/Part 3/Exercise 078/Program.cs
@@ -22,10 +22,20 @@ namespace exercise_78
         index++;
       }
       Console.WriteLine("");
-
+      
+      
       // Implement here
-      // asking for the two indices
-      // and then swapping them
+      Console.WriteLine("Give two indices to swap.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            //You didnt declare an array, lets fix that
+            int[] array = new int[] { input1, input2 };
+            //now you have the variables saved in an array you can assign them
+            input1 = array[1]; //1 is the second index so we are assigning input 2 to input 1
+            input2 = array[0]; //0 is the first index so we are assigning input 1 to input 2
+            Console.WriteLine(
+                "\n" + input1 +
+                "\n" + input2);
 
 
       // DO NOT CHANGE THE FOLLOWING CODE!
--- Exercises/Part 3/Exercise 081/Program.cs
+++ Exercises/Part 3/Exercise 081/Program.cs
@@ -9,25 +9,12 @@ namespace exercise_81
     {
       // You can test your method here
 
-      int[] numbers = new int[4];
+      string[] arrayOfStrings = {"Mike L.", "Mike P.", "Mike V."};
+double[] arrayOfFloatingpoints = {1.20, 3.14, 100.0, };
 
-      numbers[0] = 12;
-      numbers[1] = 42;
-      numbers[2] = 37;
-      numbers[3] = 9;
-
-      Console.WriteLine("The array has " + numbers.Length + " elements:");
-      for (int i = 0; i < numbers.Length; i++)
-      {
-        Console.WriteLine(numbers[i]);
-      }
-
-    }
-
-    //public static void PrintNeatly(int[] array)
-    //{
-
-    //}
+for (int i = 0; i < arrayOfFloatingpoints.Length; i++) {
+    Console.WriteLine(arrayOfStrings[i] + " " +  arrayOfFloatingpoints[i]);
+}
+}
   }
 }
-
